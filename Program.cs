b403dc3197ad13be2c0cd1708dbeac42d00ff7ac
JesusTxr <chuchito27tm@gmail.com>
2025-06-08 13:05:24 -0600

    Modifique algunos archivos y agregue Migrations para la conexion a railway

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -11,19 +13,32 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "MangaAPI", Version = "v1" });
 });
 
+// Configura CORS para permitir llamadas desde el frontend/Swagger UI
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost5188", policy =>
+    {
+        policy.WithOrigins("http://localhost:5188")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddDbContext<MangaContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
-    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Test Swagger API V1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "MangaAPI V1");
 });
 
 app.UseHttpsRedirection();
+
+// Habilita CORS con la política definida
+app.UseCors("AllowLocalhost5188");
 
 app.UseAuthorization();
 
